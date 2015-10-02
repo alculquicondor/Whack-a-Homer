d3b548058574b952b0e025d75344c8135f358a07
Aldo Culquicondor <alculquicondor@gmail.com>
2015-10-02 10:41:16 -0500

    VirtualButtons

---
+++ Assets/Scripts/HitScript.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using Vuforia;
+
+public class HitScript : MonoBehaviour, IVirtualButtonEventHandler {
+
+    private GameObject homer;
+    private bool prevPressed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        homer = transform.GetChild(0).gameObject;
+        prevPressed = false;
+        VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
+        for (int i = 0; i < vbs.Length; ++i)
+            vbs[i].RegisterEventHandler(this);
+	}
+
+    void SetVisible(bool val)
+    {
+        homer.GetComponent<Renderer>().enabled = val;
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+    }
+
+    public void OnButtonPressed(VirtualButtonAbstractBehaviour vb)
+    {
+        SetVisible(false);
+    }
+
+    public void OnButtonReleased(VirtualButtonAbstractBehaviour vb)
+    {
+        SetVisible(true);
+    }
+}
