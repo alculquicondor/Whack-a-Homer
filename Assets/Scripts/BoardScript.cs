460ab189e5b1d1842ef5a59680f6d7b0a543505d
Aldo Culquicondor <alculquicondor@gmail.com>
2015-10-02 15:41:12 -0500

    proper movement and hitting

---
+++ Assets/Scripts/BoardScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardScript : MonoBehaviour {
+
+    public float timerLenght;
+    public int homerId;
+
+    private System.Random random;
+    private float timer;
+
+	void Start ()
+    {
+        random = new System.Random();
+        timer = 0;
+	}
+	
+	void FixedUpdate ()
+    {
+        timer -= Time.fixedDeltaTime;
+        if (timer <= 0)
+        {
+            homerId = random.Next(0, 6);
+            timer = timerLenght;
+        }
+	}
+}
--- Assets/Scripts/HitScript.cs
+++ Assets/Scripts/HitScript.cs
@@ -1,39 +1,28 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using Vuforia;
 
 public class HitScript : MonoBehaviour, IVirtualButtonEventHandler {
 
-    private GameObject homer;
-    private bool prevPressed;
+    private HomerScript homerScript;
 
-	// Use this for initialization
 	void Start ()
     {
-        homer = transform.GetChild(0).gameObject;
-        prevPressed = false;
+        homerScript = GetComponentInChildren<HomerScript>();
         VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
         for (int i = 0; i < vbs.Length; ++i)
             vbs[i].RegisterEventHandler(this);
 	}
 
-    void SetVisible(bool val)
-    {
-        homer.GetComponent<Renderer>().enabled = val;
-    }
-
-	// Update is called once per frame
 	void Update ()
     {
     }
 
     public void OnButtonPressed(VirtualButtonAbstractBehaviour vb)
     {
-        SetVisible(false);
+        homerScript.Hit();
     }
 
     public void OnButtonReleased(VirtualButtonAbstractBehaviour vb)
     {
-        SetVisible(true);
     }
 }
