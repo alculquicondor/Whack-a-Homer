0ae70a4bbe007204dc4e27b2668da9f8d741c58b
Aldo Culquicondor <alculquicondor@gmail.com>
2015-10-04 11:13:45 -0500

    hammer & sound

--- Assets/Scripts/HitScript.cs
+++ Assets/Scripts/HitScript.cs
@@ -1,29 +1,67 @@
 ﻿using UnityEngine;
 using Vuforia;
 
-public class HitScript : MonoBehaviour, IVirtualButtonEventHandler {
+public class HitScript : MonoBehaviour, IVirtualButtonEventHandler, ITrackableEventHandler {
 
-    private HomerScript homerScript;
 	public AudioClip doh;
+    public int homerId;
+
+    private GameObject board;
+    private BoardScript boardScript;
+    private bool active, boardDetected, justDetected;
+    private int prevHomerId;
 
 	void Start ()
     {
-        homerScript = GetComponentInChildren<HomerScript>();
-        VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
-        for (int i = 0; i < vbs.Length; ++i)
-            vbs[i].RegisterEventHandler(this);
+        board = GameObject.Find("Board");
+        boardScript = board.GetComponent<BoardScript>();
+
+        prevHomerId = -1;
+        active = false;
+        boardDetected = false;
+        board.GetComponent<TrackableBehaviour>().RegisterTrackableEventHandler(this);
+        GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
 	}
 
-	void Update ()
+    void SetVisible(bool val)
     {
+        GetComponentInChildren<Renderer>().enabled = boardDetected && active && val;
     }
 
     public void OnButtonPressed(VirtualButtonAbstractBehaviour vb)
     {
-        homerScript.Hit();
+        if (!boardDetected || ! active)
+            return;
+        SetVisible(false);
+		GetComponent<AudioSource>().clip = doh;
+		GetComponent<AudioSource> ().PlayOneShot(GetComponent<AudioSource>().clip);
     }
 
     public void OnButtonReleased(VirtualButtonAbstractBehaviour vb)
     {
+    }
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        boardDetected = newStatus >= TrackableBehaviour.Status.DETECTED;
+        if (previousStatus < TrackableBehaviour.Status.DETECTED && newStatus >= TrackableBehaviour.Status.DETECTED)
+            justDetected = true;
+    }
+
+	void FixedUpdate ()
+    {
+        justDetected = false;
+        if ((prevHomerId != homerId) == (boardScript.homerId == homerId))
+        {
+            active = boardScript.homerId == homerId;
+            SetVisible(active);
+        }
+        prevHomerId = boardScript.homerId;
+	}
+
+    void LateUpdate ()
+    {
+        if (justDetected)
+            SetVisible(false);
     }
 }
