155155cbb5d6536390f5b89631a4e9a405eec087
Aldo Culquicondor <alculquicondor@gmail.com>
2015-10-30 11:31:12 -0500

    animations on start screen

--- Assets/Scripts/Click.cs
+++ Assets/Scripts/Click.cs
@@ -4,9 +4,12 @@ using System.Collections;
 public class Click : MonoBehaviour {
 
 	public AsyncOperation op;
+    public GameObject hommer;
+    private float time;
 
 	void Start () {
         StartCoroutine(LoadBoard());
+        time = 0;
 	}
 
     IEnumerator LoadBoard()
@@ -20,5 +23,9 @@ public class Click : MonoBehaviour {
         if (Cardboard.SDK.Triggered) {
             op.allowSceneActivation = true;
         }
+        time += Time.deltaTime;
+        hommer.transform.Translate(new Vector3(-Time.deltaTime * 15, 0.15f * Mathf.Sin(10 * time), 0));
+        if (hommer.transform.position.x < -40)
+            hommer.transform.Translate(new Vector3(80, 0, 0));
 	}
 }
